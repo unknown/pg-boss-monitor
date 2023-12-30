5bb3ace481a267fc1fc896f58f091a084b0acb7b
David Mo <44306450+unknown@users.noreply.github.com>
2023-12-30 02:20:11 -0500

    Use tanstack table

--- apps/client/src/components/job-table.tsx
+++ apps/client/src/components/job-table.tsx
@@ -1,16 +1,12 @@
 "use client";
 
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { trpc } from "@/trpc/react";
 import { useState } from "react";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import { DataTable } from "@/components/ui/data-table";
+import { trpc } from "@/trpc/react";
 
 const JobStates = [
   "created",
@@ -26,6 +22,41 @@ type JobState = (typeof JobStates)[number];
 interface JobsTableProps {
   queue: string;
 }
+
+type Job = {
+  id: string;
+  data: Record<string, unknown>;
+  completedon: string | null;
+  startedon: string | null;
+};
+
+const columns: ColumnDef<Job>[] = [
+  {
+    accessorKey: "id",
+    header: "ID",
+  },
+  {
+    accessorKey: "data",
+    cell: (info) => <pre>{JSON.stringify(info.getValue(), null, 2)}</pre>,
+    header: "Data",
+  },
+  {
+    accessorKey: "completedon",
+    header: "Completed",
+  },
+  {
+    accessorFn: (row) => {
+      if (!row.completedon || !row.startedon) {
+        return null;
+      }
+      const startedonDate = new Date(row.startedon);
+      const completedonDate = new Date(row.completedon);
+      const duration = completedonDate.getTime() - startedonDate.getTime();
+      return (duration / 1000).toFixed(2);
+    },
+    header: "Duration (seconds)",
+  },
+];
 
 export function JobsTable({ queue }: JobsTableProps) {
   const [selectedState, setSelectedState] = useState<JobState>("completed");
@@ -44,51 +75,11 @@ export function JobsTable({ queue }: JobsTableProps) {
       >
         {JobStates.map((jobState) => (
           <ToggleGroupItem key={jobState} value={jobState}>
-            {jobState === selectedState && jobs.data
-              ? `${jobState} (${jobs.data.length})`
-              : jobState}
+            {jobState}
           </ToggleGroupItem>
         ))}
       </ToggleGroup>
-      <Table>
-        <TableHeader className="sticky top-0 bg-background">
-          <TableRow>
-            <TableHead>ID</TableHead>
-            <TableHead>Data</TableHead>
-            <TableHead>Completed</TableHead>
-            <TableHead className="text-right w-[200px]">
-              Duration (seconds)
-            </TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {jobs.data?.map((job) => {
-            const { id, data, completedon, startedon } = job;
-            const completedonDate = completedon ? new Date(completedon) : null;
-            const startedonDate = startedon ? new Date(startedon) : null;
-            const duration =
-              completedonDate && startedonDate
-                ? (
-                    (completedonDate.getTime() - startedonDate.getTime()) /
-                    1000
-                  ).toFixed(2)
-                : null;
-
-            return (
-              <TableRow key={id} className="max-h-40">
-                <TableCell>{id}</TableCell>
-                <TableCell>
-                  <pre>{JSON.stringify(data, null, 2)}</pre>
-                </TableCell>
-                <TableCell title={completedon ?? undefined}>
-                  {completedonDate ? completedonDate.toLocaleString() : null}
-                </TableCell>
-                <TableCell className="text-right">{duration}</TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table>
+      {jobs.data && <DataTable columns={columns} data={jobs.data} />}
     </>
   );
 }
