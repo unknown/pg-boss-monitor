b77086df8e7934757b181426192fd036b2354132
David Mo <44306450+unknown@users.noreply.github.com>
2024-01-07 21:59:40 -0500

    Improve table

--- apps/client/src/components/job-table.tsx
+++ apps/client/src/components/job-table.tsx
@@ -4,9 +4,10 @@ import { useState } from "react";
 
 import { ColumnDef } from "@tanstack/react-table";
 
+import { DataTable } from "@/components/ui/data-table";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
-import { VirtualizedDataTable } from "@/components/ui/virtualized-data-table";
 import { trpc } from "@/trpc/react";
+import { Button } from "@/components/ui/button";
 
 const JobStates = [
   "created",
@@ -24,24 +25,12 @@ interface JobsTableProps {
 }
 
 type Job = {
-  id: string;
   data: Record<string, unknown>;
   completedon: string | null;
   startedon: string | null;
 };
 
 const columns: ColumnDef<Job>[] = [
-  {
-    accessorKey: "id",
-    header: "ID",
-    size: 400,
-  },
-  {
-    accessorKey: "data",
-    cell: (info) => <pre>{JSON.stringify(info.getValue(), null, 2)}</pre>,
-    header: "Data",
-    size: 400,
-  },
   {
     accessorFn: (row) => {
       if (!row.completedon) {
@@ -51,6 +40,24 @@ const columns: ColumnDef<Job>[] = [
     },
     header: "Completed",
     size: 400,
+  },
+  {
+    accessorKey: "data",
+    cell: (info) => (
+      <pre className="max-h-64 max-w-[540px] overflow-auto border p-2 rounded-md bg-muted/50">
+        {JSON.stringify(info.getValue(), null, 2)}
+      </pre>
+    ),
+    header: "Data",
+  },
+  {
+    accessorKey: "output",
+    cell: (info) => (
+      <pre className="max-h-64 max-w-[540px] overflow-auto border p-2 rounded-md bg-muted/50">
+        {JSON.stringify(info.getValue(), null, 2)}
+      </pre>
+    ),
+    header: "Output",
   },
   {
     accessorFn: (row) => {
@@ -69,30 +76,65 @@ const columns: ColumnDef<Job>[] = [
 
 export function JobsTable({ queue }: JobsTableProps) {
   const [selectedState, setSelectedState] = useState<JobState>("completed");
+  const limit = 25;
+  const [offset, setOffset] = useState(0);
 
   const jobs = trpc.pgBoss.getJobs.useQuery({
+    queue,
+    limit,
+    offset,
+    state: selectedState,
+  });
+
+  const jobsCount = trpc.pgBoss.countJobs.useQuery({
     queue,
     state: selectedState,
   });
 
   return (
     <div className="flex flex-col h-full">
-      <ToggleGroup
-        className="p-2"
-        type="single"
-        value={selectedState}
-        onValueChange={(state: JobState) => setSelectedState(state)}
-      >
-        {JobStates.map((jobState) => (
-          <ToggleGroupItem key={jobState} value={jobState}>
-            {jobState}
-          </ToggleGroupItem>
-        ))}
-      </ToggleGroup>
-      {jobs.data === undefined ? (
-        "Loading..."
-      ) : (
-        <VirtualizedDataTable columns={columns} data={jobs.data} />
+      <div className="flex items-center p-2 gap-2">
+        <ToggleGroup
+          type="single"
+          value={selectedState}
+          onValueChange={(state: JobState) => {
+            setSelectedState(state);
+            setOffset(0);
+          }}
+        >
+          {JobStates.map((jobState) => (
+            <ToggleGroupItem key={jobState} value={jobState}>
+              {jobState}
+              {selectedState === jobState &&
+                ` (${jobsCount.isLoading ? "..." : jobsCount.data})`}
+            </ToggleGroupItem>
+          ))}
+        </ToggleGroup>
+      </div>
+      <div className="flex items-center p-2 gap-2">
+        <Button
+          variant="outline"
+          onClick={() => setOffset((offset) => Math.max(0, offset - limit))}
+          disabled={offset === 0}
+        >
+          {"<"}
+        </Button>
+        <p className="tabular-nums">{limit}</p>
+        <p className="tabular-nums">{offset}</p>
+        <Button
+          variant="outline"
+          onClick={() =>
+            setOffset((offset) =>
+              Math.min(offset + limit, jobsCount.data ?? limit)
+            )
+          }
+          disabled={offset + limit >= (jobsCount.data ?? limit)}
+        >
+          {">"}
+        </Button>
+      </div>
+      {jobs.data === undefined ? null : (
+        <DataTable columns={columns} data={jobs.data} />
       )}
     </div>
   );
--- packages/api/src/router/pg-boss.ts
+++ packages/api/src/router/pg-boss.ts
@@ -26,16 +26,32 @@ export const PgBossRouter = router({
       z.object({
         queue: z.string(),
         state: z.enum(JobStates),
-        limit: z.number().optional(),
+        limit: z.number().default(50),
+        offset: z.number().default(0),
       })
     )
     .query(async ({ ctx, input }) => {
       const { rows } = await ctx.db.query(
-        `select * from ${schema}.job where name = $1 and state = $2::${schema}.job_state order by completedon desc;`,
-        [input.queue, input.state]
+        `select * from ${schema}.job where name = $1 and state = $2::${schema}.job_state order by completedon desc limit $3 offset $4;`,
+        [input.queue, input.state, input.limit, input.offset]
       );
 
       return rows as JobWithMetadata[];
+    }),
+  countJobs: publicProcedure
+    .input(
+      z.object({
+        queue: z.string(),
+        state: z.enum(JobStates),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { rows } = await ctx.db.query(
+        `select count(*) from ${schema}.job where name = $1 and state = $2::${schema}.job_state;`,
+        [input.queue, input.state]
+      );
+
+      return rows[0].count as number;
     }),
   getQueues: publicProcedure.query(async ({ ctx }) => {
     const { rows } = await ctx.db.query(
